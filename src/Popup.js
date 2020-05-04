76cbc634a83e16b787ed51377f126164f91a8bd0
Manuel Dalle <manuel.dalle@dashlane.com>
2020-05-04 20:04:53 +0200

    remove npm package jsontotable to use custom component instead

--- src/Popup.js
+++ src/Popup.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useEffect } from "react";
 import { _Store } from "./Store";
 import StoreContext from "./StoreContext";
-// import { JsonToTable } from "react-json-to-table";
 import { JsonToTable } from "./JsonToTable";
 
 export const Popup = () => {
