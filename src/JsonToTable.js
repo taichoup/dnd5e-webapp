7f6bb7b9f43b7126858578ead41a96364943019d
Manuel Dalle <manuel.dalle@dashlane.com>
2020-05-04 19:52:15 +0200

    finally made it work

--- src/JsonToTable.js
+++ src/JsonToTable.js
@@ -3,35 +3,74 @@ import "./JsonToTable.css";
 
 export const JsonToTable = (props) => {
 
-    // const isEven = (a) => 0 === a % 2;
+    const isEven = (a) => 0 === a % 2;
 
     const isArray = (a) => Array.isArray(a);
 
-    const isPrimitive = (a) => "object" !== typeof a; // enough for our purposes here
+    const isPrimitive = (a) => "object" !== typeof a;
 
     const isObject = (a) => !isArray(a) && !isPrimitive(a);
+
+    /*
+    * This functions tries to handle arrays of objects in a visually 
+    * aesthetic way (keys as column labels), even when the objects 
+    * don't all have the same keys (that's the tricky part)
+    */
+    const buildArrayOfObjects = (arr) => {
+
+        const allKeys = arr.map(e => Object.keys(e));
+        const allKeysFlat = [].concat.apply([], allKeys);
+        const allKeysSet = new Set(allKeysFlat);
+        const thead = Array.from(allKeysSet);
+
+        return (
+            <table className="small-table">
+                <thead>
+                    <tr>
+                        {thead.map(e => <th>{e}</th>)}
+                    </tr>
+                </thead>
+                <tbody>
+                    {arr.map((e, i) =>
+                        <tr>{thead.map((k, j) =>
+                            <td className={isEven(i)
+                                ? "td_row_even"
+                                : "td_row_odd"}>
+                                {e.hasOwnProperty(k)
+                                    ? isObject(e[k])
+                                        ? buildTable(e[k])
+                                        : isArray(e[k])
+                                            ? buildArray(e[k])
+                                            : e[k]
+                                    : null}
+                            </td>)}
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        )
+    }
 
 
     /*
     * takes the data and returns the final table
     */
-    const buildTable = (data, className) => {
+    const buildTable = (data) => {
 
         if (isArray(data)) {
-            return buildArray(data, className);
+            return buildArray(data);
         }
         console.log(data);
         return (
-            <table className={className}>
+            <table style={{ width: "100%" }}>
                 <tbody>
-
                     {
                         // json is not an array. iterating over its properties
                         data // result of async operation so need to check that it's there for intermediate renders
                             ? Object.keys(data).map(c =>
 
                                 (isPrimitive(data[c]) || isArray(data[c]))
-                                    // case where the value is an array or string
+                                    // case where the value is an array or a primitive
                                     ? (isArray(data[c]))
 
                                         // value is an array
@@ -40,11 +79,11 @@ export const JsonToTable = (props) => {
                                                 <div className="td_head">
                                                     {c}
                                                 </div>
-                                                {buildArray(data[c], "small-table")}
+                                                {buildArray(data[c])}
                                             </td>
                                         </tr>
 
-                                        // value is a string or int (end leaf)
+                                        // value is a primitive (end leaf)
                                         : <tr>
                                             <td>
                                                 <div className="td_head">
@@ -64,7 +103,7 @@ export const JsonToTable = (props) => {
                                             <div className="td_head">
                                                 {c}
                                             </div>
-                                            {buildTable(data[c], "small-table")}
+                                            {buildTable(data[c])}
                                         </td>
                                     </tr>
                             )
@@ -75,71 +114,47 @@ export const JsonToTable = (props) => {
         )
     }
 
-    const buildArray = (arr, className) => {
+    const buildArray = (arr) => {
 
         // empty array, return empty div
         if (arr.length === 0) {
-            console.log("empty array");
-            return <div></div>
+            return <div style={{ fontSize: "small" }}> None.</div >
         }
 
         return (
-            <table className={className}>
-                <tbody>
+            isObject(arr[0]) // assuming type homogenity inside arrays (if first elt is obj, all are.)
 
-                    {isObject(arr[0]) // assuming type homogenity inside arrays (if first elt is obj, all are.)
+                ? buildArrayOfObjects(arr)
 
-                        // object
-                        ? <table>
-                            <thead>
-                                {arr.map((item, idx) => idx === 0
-                                    ? <tr>{
-                                        Object.keys(item).map(i =>
-                                            <th>
-                                                <div className="td_head">
-                                                    {i}
-                                                </div>
-                                            </th>
-                                        )
-                                    }</tr>
-                                    : null)
-                                }
-                            </thead>
-                            <tbody>
-                                {arr.map((item, idx) =>
-                                    <tr>{Object.values(item).map(i =>
-                                        <td className={idx % 2 === 0 ? "td_row_even" : "td_row_odd"}>{isObject(i) ? null : i.toString()}</td>)
-                                    }</tr>)}
-                            </tbody>
-                        </table>
+                // array and primitive
+                : isArray(arr[0])
 
-
-                        // array and primitive
-                        : <tr>
+                    // array
+                    ? <table>
+                        <tr>
                             {
                                 arr.map((item) =>
-                                    (isArray(item))
-
-                                        // array 
-                                        ? <td colSpan="2">
-                                            <div className="td_head"></div>
-                                            {buildArray(item, className)}
-                                        </td>
-
-                                        // string
-                                        : <td>
-                                            <div className="td_head">
-                                                &nbsp;
-                                            </div>
-                                        </td>)
+                                    <td colSpan="2">
+                                        <div className="td_head"></div>
+                                        {buildArray(item)}
+                                    </td>
+                                )
                             }
                         </tr>
-                    }
+                    </table>
 
-                </tbody>
-            </table>
+                    // string
+                    : <ul className="api-results-ul">
+                        {arr.map(item => <li>{item}</li>)}
+                    </ul>
+
+
         )
     }
 
-    return buildTable(props.json, "api-results");
+    return (
+        <div className="api-results">
+            {buildTable(props.json, "api-results")}
+        </div>
+    )
 }
