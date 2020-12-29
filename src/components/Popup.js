917872018e69fcbadacf7cd2721da39ce13bf598
Manuel Dalle <manuel.dalle@dashlane.com>
2020-12-30 00:11:48 +0100

    clean up the clutter from api response

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -4,6 +4,7 @@ import { _Store } from "../Store";
 import StoreContext from "../StoreContext";
 import { JsonToTable } from "./JsonToTable";
 import { Loader } from "./Loader";
+import { removeUselessProperties, removeNullishValues } from "../utils/apiFormatter";
 
 export const Popup = () => {
   useEffect(() => {
@@ -12,6 +13,11 @@ export const Popup = () => {
       try {
         const res = await(fetch(statePath));
         const res_json = await res.json();
+        console.log("res_json: ", res_json);
+        removeUselessProperties(res_json, "url");
+        removeUselessProperties(res_json, "index");
+        removeNullishValues(res_json);
+        console.log("cleaned", res_json);
         _Store.dispatch({ type: "GETDATA_POPUP", payload: res_json });
       } catch (error) {
         console.log(error.message);
